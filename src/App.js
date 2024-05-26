9a718d85c1985005d2930c9bb3480325f02a0063
Vivek Chavan <95691809+Vivek7038@users.noreply.github.com>
2024-05-26 11:11:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useCallback } from "react";
+import "./styles.css";
+import PaginationPage from "./pages/Question2";
+
+const App = () => {
+  return (
+    <>
+      <PaginationPage />
+    </>
+  );
+};
+
+export default App;
